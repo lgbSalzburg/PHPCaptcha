473484e5c689a688744d83064d6be41f590c1b00
MartinWiesinger <martin.wiesinger@stud.sbg.ac.at>
2022-09-12 18:30:51 +0200

    include loop.py

--- py/create.py
+++ py/create.py
@@ -7,6 +7,7 @@ import random
 import string
 import hashlib
 import os
+import sys
 
 
 def get_random_string(length):
@@ -15,43 +16,52 @@ def get_random_string(length):
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str
 
-text = get_random_string(6)
+def create():
+    text = get_random_string(6)
 
-text = text.replace("q","g")
+    text = text.replace("q","g")
 
-text = text.replace("l","i")
+    text = text.replace("l","i")
 
-h = hashlib.new('sha256')
+    h = hashlib.new('sha256')
 
-toHash = text.encode()
-h.update(toHash)
+    toHash = text.encode()
+    h.update(toHash)
 
-h = h.hexdigest()
+    h = h.hexdigest()
 
-print(h)
-print(text)
+    print(h)
+    print(text)
 
-text = ' '.join(text).strip() 
- 
-# creating a image object (new image object) with
-# RGB mode and size 200x200
-img = PIL.Image.new(mode = "RGB", size = (300, 100),
-                           color = (205, 205, 205))
-draw = ImageDraw.Draw(img)
-draw.rectangle((0, 0, 299, 99), outline='black')
-# specified font size
-font = ImageFont.truetype(r'fonts/Buggie-L3y03.ttf', 40) 
+    text = ' '.join(text).strip() 
+    
+    # creating a image object (new image object) with
+    # RGB mode and size 200x200
+    img = PIL.Image.new(mode = "RGB", size = (300, 100),
+                            color = (205, 205, 205))
+    draw = ImageDraw.Draw(img)
+    draw.rectangle((0, 0, 299, 99), outline='black')
+    # specified font size
+    font = ImageFont.truetype(r'../fonts/Buggie-L3y03.ttf', 40) 
 
-draw.text((35, 30),text,fill="green", font = font, align="center")
+    draw.text((35, 30),text,fill="green", font = font, align="center")
 
-#draw some lines 
-draw.ellipse([(30, 0), (500, 200)], fill=None, outline='green', width=2)
+    #draw some lines 
+    draw.ellipse([(30, 0), (500, 200)], fill=None, outline='green', width=2)
 
-draw.ellipse([(150, -50), (800, 200)], fill=None, outline='green', width=2)
+    draw.ellipse([(150, -50), (800, 200)], fill=None, outline='green', width=2)
 
-if not os.path.exists('img/'):
-    os.mkdir("img/")
+    if not os.path.exists('../img/'):
+        os.mkdir("../img/")
 
 
-img.save('img/'+h+'.jpg')
-h = ""
+    img.save('../img/'+h+'.jpg')
+    h = ""
+
+if(len(sys.argv) == 1):
+    count = 1
+else:
+    count = int(sys.argv[1])
+
+for x in range(0, count):
+  create()
